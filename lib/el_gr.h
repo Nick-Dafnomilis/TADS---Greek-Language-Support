59c3cb65556c0b11993811a30e81b04b528b150d
Nick-Dafnomilis <107199330+Nick-Dafnomilis@users.noreply.github.com>
2025-06-30 10:57:21 +0300

    Add files via upload

---
+++ lib/el_gr.h
@@ -0,0 +1,15 @@
+#charset "iso-8859-7"
+
+// Greek TADS Library Header File
+#include <greek_article_case.t>
+#include <greek_gram.t>
+#include <greek_grammar_cases.t>
+#include <greek_grammar_conversation.t>
+#include <greek_grammar_core.t>
+#include <greek_grammar_misc.t>
+#include <greek_language.t>
+#include <greek_messages.t>
+#include <greek_msg.t>
+#include <greek_ne.t>
+#include <greek_parser_ext.t>
+#include <greek_verb_tense.t>
